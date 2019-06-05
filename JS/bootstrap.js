cc2c6f4114dab514917f0bb28b15096325692587
costagavras <cgavras@yahoo.ca>
2019-06-04 22:24:15 -0400

    timer fixed

--- JS/bootstrap.js
+++ JS/bootstrap.js
@@ -54,15 +54,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
       var clock;
       // Set dates.
-      var futureDate  = new Date("Fri, 07 Jun 2019 09:00:00 +0300");
-      var currentDate = new Date();
+      // var futureDate  = new Date("Fri, 07 Jun 2019 09:00:00 +0300");
+      // var currentDate = new Date();
+      var currentDate  = new Date();
+      var pastDate = new Date("00:00:00");
       // Calculate the difference in seconds between the future and current date
-      var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+      var diff = currentDate.getTime() / 1000 - pastDate.getTime() / 1000;
       // Calculate day difference and apply class to .clock for extra digit styling.
       function dayDiff(first, second) {
           return (second-first)/(1000*60*60*24);
       }
-      if (dayDiff(currentDate, futureDate) < 100) {
+      if (dayDiff(currentDate, pastDate) < 100) {
           $('#actionTimer').addClass('twoDayDigits');
       } else {
           $('#actionTimer').addClass('threeDayDigits');
@@ -73,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function(){
       // Instantiate a countdown FlipClock
       clock = $('#actionTimer').FlipClock(diff, {
           clockFace: 'DailyCounter',
-          countdown: true,
+          countdown: false,
           language: 'english'
       });
 
